406a16c1141b4b427b15c33f4d913aea281bdaa2
zraykeel737 <zraykeel737@SDS-L-DLFDF63>
2022-09-15 10:54:24 -0400

    Add project files.

---
+++ NuclearReactor/Form1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace NuclearReactor
+{
+    public partial class Form1 : Form
+    {
+        public Form1() => InitializeComponent();
+        private void Button1_Click(object sender, EventArgs e)
+
+
+    }
+}
